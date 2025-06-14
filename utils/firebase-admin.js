a5178f59fb20d1853ac0f1889bc53bb63a5be8cd
Kevinb59 <130297610+Kevinb59@users.noreply.github.com>
2025-06-14 15:45:04 +0200

    MAJ Auto Cursor

--- utils/firebase-admin.js
+++ utils/firebase-admin.js
@@ -3,9 +3,14 @@ import admin from 'firebase-admin'
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n')
+      project_id: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_ADMIN_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(
+        /\\\\n/g,
+        '\n'
+      ),
+      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_ADMIN_CLIENT_ID
     })
   })
 }
